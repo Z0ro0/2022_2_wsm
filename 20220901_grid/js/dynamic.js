d48c03a8804c58959671c63f5122ad01348117d7
Z0ro0 <chlrkdmf0805@gmail.com>
2022-09-22 14:06:27 +0900

    년, 월, 일 구하는 거 함수로 만들기

--- 20220901_grid/js/dynamic.js
+++ 20220901_grid/js/dynamic.js
@@ -1,3 +1,18 @@
+const setCalendar= (year, month) => {
+    //오늘 년, 월, 1 날짜 객체 구하기
+    let firstDate = new Date(year, month - 1, 1);
+    
+    //그 객체 요일 구하기
+    let firstDay = firstDate.getDay();
+
+    console.log(`${year}-${month}-${firstDay}요일`);
+
+    //html 제어하기
+    //오늘 년, 오늘 월+1, 0 날짜 객체 구하기 --> 이번 달 마지막 일 구하기
+    let lastDate = new Date(year, month, 0).getDate();
+    console.log(`${lastDate}`);
+}
+
 //오늘 구하기
 let today = new Date();
 //오늘 연 구하기
@@ -5,16 +20,6 @@ let year = today.getFullYear();
 //오늘 월 구하기
 let month = today.getMonth();
 month++;
-//오늘 년, 월, 1 날짜 객체 구하기
-let firstDate = new Date(year, month - 1, 1);
-//그 객체 요일 구하기
-
-let firstDay = firstDate.getDay();
-
-console.log(`${year}-${month}-${firstDay}요일`);
 
 
-//html 제어하기
-//오늘 년, 오늘 월+1, 0 날짜 객체 구하기 --> 이번 달 마지막 일 구하기
-let lastDate = new Date(year, month, 0).getDate();
-console.log(`${lastDate}`);
+setCalendar(year, month);
