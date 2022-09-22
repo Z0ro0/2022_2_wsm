197a17e3aa8356b3dfb15c27644abb84f4252cfb
Z0ro0 <chlrkdmf0805@gmail.com>
2022-09-22 14:01:32 +0900

    js 현재 달의 말일 구하기

---
+++ 20220901_grid/js/dynamic.js
@@ -0,0 +1,20 @@
+//오늘 구하기
+let today = new Date();
+//오늘 연 구하기
+let year = today.getFullYear();
+//오늘 월 구하기
+let month = today.getMonth();
+month++;
+//오늘 년, 월, 1 날짜 객체 구하기
+let firstDate = new Date(year, month - 1, 1);
+//그 객체 요일 구하기
+
+let firstDay = firstDate.getDay();
+
+console.log(`${year}-${month}-${firstDay}요일`);
+
+
+//html 제어하기
+//오늘 년, 오늘 월+1, 0 날짜 객체 구하기 --> 이번 달 마지막 일 구하기
+let lastDate = new Date(year, month, 0).getDate();
+console.log(`${lastDate}`);
