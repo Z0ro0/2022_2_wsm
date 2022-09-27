bdc8579622363c87f3294907bbc5327584d59e3e
Z0ro0 <chlrkdmf0805@gmail.com>
2022-09-27 14:04:39 +0900

    급식 오픈 api 불러오는 방법

---
+++ 20220901_grid/js/openapi.js
@@ -0,0 +1,21 @@
+//시도교육청 코드 B10: 서울특별시교육청
+// 표준학교 코드 7010569
+//식사코드: 2: 중식
+
+//KEY:
+// ATPT_OFCDC_SC_CODE	     STRING(필수)	    시도교육청코드
+//     SD_SCHUL_CODE	     STRING(필수)	    표준학교코드
+//     MMEAL_SC_CODE	     STRING(선택)	    식사코드
+//     MLSV_YMD	     STRING(선택)	    급식일자
+//     MLSV_FROM_YMD	     STRING(선택)	    급식시작일자
+//     MLSV_TO_YMD	     STRING(선택)	    급식종료일자
+
+//  https://open.neis.go.kr/hub/mealServiceDietInfo?ATPT_OFCDC_SC_CODE=B10&SD_SCHUL_CODE=7010569&MMEAL_SC_CODE=2&MLSV_YMD=20220927&Type=json
+
+
+
+const KEY = "f9f06af16f3449b6860a8b365af1f232";
+const ATPT_OFCDC_SC_CODE = "B10";
+const SD_SCHUL_CODE = "7010569";
+let MMEAL_SC_CODE = 2;
+let MLSV_YMD = "20220927";
